341bf08a3c146342f2e71778d375ab8a942ec8ed
Pascal Sanger <psanger@2-C-3.42heilbronn.de>
2024-07-16 15:46:11 +0200

    add contact

--- CPP00/ex01/phonebook.cpp
+++ CPP00/ex01/phonebook.cpp
@@ -6,19 +6,61 @@
 /*   By: psanger <psanger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 01:59:55 by psanger           #+#    #+#             */
-/*   Updated: 2024/07/05 01:59:57 by psanger          ###   ########.fr       */
+/*   Updated: 2024/07/16 15:45:19 by psanger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-phonebook::phonebook(){}
-phonebook::~phonebook(){}
+phonebook::phonebook()
+{
+	this->index = 0;
+	std::cout << "Phonebook created, can store up to 8 contacts\n";
+}
+
+phonebook::~phonebook()
+{
+	std::cout << "Phonebook destroyed\n";
+}
 
 void phonebook::addContact()
 {
-	std::cout << "add contact" << std::endl;
+	std::string input;
 
+	std::cout << "Enter your First Name: " << std::endl;
+	std::cin >> input;
+	if (std::cin.eof())
+		exit(1);
+	this->contacts[index].setFirstName(input);
+
+	std::cout << "Enter your Last Name: " << std::endl;
+	std::cin >> input;
+	if (std::cin.eof())
+		exit(1);
+	this->contacts[index].setLastName(input);
+
+	std::cout << "Enter your Nick Name: " << std::endl;
+	std::cin >> input;
+	if (std::cin.eof())
+		exit(1);
+	this->contacts[index].setNickName(input);
+
+	std::cout << "Enter your Phonenumber: " << std::endl;
+	std::cin >> input;
+	if (std::cin.eof())
+		exit(1);
+	this->contacts[index].setPhoneNumber(input);
+
+	std::cout << "Enter your darkest secret: " << std::endl;
+	std::cin >> input;
+	if (std::cin.eof())
+		exit(1);
+	this->contacts[index].setDarkSecret(input);
+
+	this->index++;
+
+	if (this->index > 8)
+		this->index = 0;
 }
 
 void phonebook::searchContact()
