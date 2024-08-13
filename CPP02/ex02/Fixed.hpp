09780fb09200db6bc0ff9b816f5ead6507b0d225
Pascal Sanger <psanger@2-E-1.42heilbronn.de>
2024-08-13 16:20:02 +0200

    CPP02 ex02 operator and min max

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psanger <psanger@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 16:46:51 by psanger           #+#    #+#             */
+/*   Updated: 2024/08/13 16:12:04 by psanger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int 				fixedPointNum;
+	static const int	fractionalBits = 8;
+public:
+	Fixed();
+	Fixed(const int num_int);
+	Fixed(const float num_float);
+
+	Fixed(const Fixed& other);
+
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+
+	bool operator<(const Fixed& other) const;
+	bool operator<=(const Fixed& other) const;
+	bool operator>(const Fixed& other) const;
+	bool operator>=(const Fixed& other) const;
+	bool operator==(const Fixed& other) const;
+	bool operator!=(const Fixed& other) const;
+
+	Fixed operator+(const Fixed& other) const;
+	Fixed operator-(const Fixed& other) const;
+	Fixed operator*(const Fixed& other) const;
+	Fixed operator/(const Fixed& other) const;
+
+	Fixed operator++(void);
+	Fixed operator++(int);
+	Fixed operator--(void);
+	Fixed operator--(int);
+
+	static Fixed& max(Fixed& one, Fixed& two);
+	static Fixed& min(Fixed& one, Fixed& two);
+	static const Fixed& max(const Fixed& one, const Fixed& two);
+	static const Fixed& min(const Fixed& one, const Fixed& two);
+};
+
+std::ostream & operator<<( std::ostream & o, Fixed const & i );
---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psanger <psanger@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 16:46:10 by psanger           #+#    #+#             */
+/*   Updated: 2024/08/13 16:13:21 by psanger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+	// Fixed const a( 10 );
+	// Fixed const b( 42.42f );
+	// Fixed const c = a - b;
+	// Fixed const d = a * b;
+	// Fixed const e = b / a;
+	// Fixed		f;
+
+
+	// std::cout << std::endl << c << std::endl;
+	// std::cout << std::endl << d << std::endl;
+	// std::cout << std::endl << e << std::endl;
+
+
+	// std::cout << std::endl << f << std::endl << std::endl;
+
+	// std::cout << Fixed::min( a, b ) << std::endl << std::endl;
+
+	// std::cout << f << std::endl;
+	// std::cout << ++f << std::endl;
+	// std::cout << f << std::endl;
+	// // std::cout << f++ << std::endl;
+	// std::cout << f << std::endl;
+
+	// if (b > c)
+	// 	std::cout << "\033[34mPasst!\033[0m\n";
+	// if (b < c)
+	// 	std::cout << "\033[34mPasstNicht!\033[0m\n";
+	// return (0);
+
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return 0;
+}
