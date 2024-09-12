f673d7f1572cea67615a331220b39bea57fc7cc4
Pascal Sanger <psanger@2-E-5.42heilbronn.de>
2024-09-12 22:06:02 +0200

    CPP 01 ex00 change in main

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,17 +6,21 @@
 /*   By: psanger <psanger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 17:16:29 by psanger           #+#    #+#             */
-/*   Updated: 2024/09/05 17:39:39 by psanger          ###   ########.fr       */
+/*   Updated: 2024/09/12 22:05:40 by psanger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int main(int argc, char **argv, char **envp)
+int main()
 {
 	Zombie pascal("pascal");
-	Zombie *jan = newZombie("jan");
 	pascal.announce();
+
+
+	Zombie *jan = newZombie("jan");
+	jan->announce();
+
 	randomChump("Maxus");
 	delete (jan);
 }
